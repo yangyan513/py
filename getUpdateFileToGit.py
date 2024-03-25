391fc049d53afdf131d2b6506f616863d478c46b
阳艳 <yan.yang@goland.cn>
2024-03-25 18:26:05 +0800

    [fix] 优化点： 解决文件名修改检测不出来； 新增删除文件功能（a项目文件删除，b项目也会被删除）； 新增结束时间，不填写默认为当前时间（主要针对一个需求上线一段时间，后期需要同步的情况）。 主站和streamfab.us同步 解决了pages下面存在目录结构的差异（eg:streamfab.us站pages/main/checkout.vue同步到dvdfab_web站，会自动同步到pages/checkout.vue下，dvdfab_web往streamfab.us相反推理。） streamfab.us 的活动使用promotion.htm, 主站则存在两个活动 promotion.htm 和 streamfab-promotion.htm（配置文件streamfab_promotion_info.json）, 从streamfab.us 同步时会promotion.vue和promotion_info.json会修改为streamfab_promotion.vue和streamfab_promotion_info.json。（要求streamfab.us的活动代码做兼容，同步到dvdfab_web不做任何改动。）

--- getUpdateFileToGit.py
+++ getUpdateFileToGit.py
@@ -4,57 +4,85 @@
 from git import Repo
 from datetime import datetime
 from functools import reduce
-from dateutil import tz  # 新增的库
+from dateutil import tz
 
-def list_changed_files_after_date_by_authors(repo_path, branch, authors, date):
+def list_changed_files_after_date_by_authors(repo_path, branch, authors, start_date, end_date):
     commit_authors = authors.split(',')
-    date_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-    date_obj = date_obj.replace(tzinfo=tz.UTC)
+    start_date_obj = datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S').replace(tzinfo=tz.UTC)
+    if end_date == '':
+        end_date_obj = datetime.now().replace(tzinfo=tz.UTC)
+    else:
+        end_date_obj = datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S').replace(tzinfo=tz.UTC)
+    
     repo = Repo(repo_path)
     commits = list(repo.iter_commits(branch))
+    
     changed_files = set()
+    deleted_files = []  # 用于记录被删除的文件路径
+    
     for commit in commits:
-        
-        if 'Merge' in commit.message:  # 加这一句
+        if 'Merge' in commit.message:
             continue
-        if commit.authored_datetime >= date_obj and commit.author.name in commit_authors:
+        
+        if start_date_obj <= commit.authored_datetime <= end_date_obj and commit.author.name in commit_authors:
             for parent in commit.parents:
                 diffs = parent.diff(commit)
-                for diff in diffs.iter_change_type('M'):  # Modified files
-                    changed_files.add(diff.a_path)
-                for diff in diffs.iter_change_type('A'):  # Added files
-                    changed_files.add(diff.a_path)
-    return list(changed_files)
+                for diff in diffs:
+                    if diff.change_type in ['M', 'A']:  # Modified, Added files
+                        changed_files.add(diff.a_path)
+                    if diff.change_type in ['R']:  # Renamed files
+                        changed_files.add(diff.b_path)
+                    elif diff.change_type == 'D':  # Deleted files
+                        changed_files.add(diff.a_path)  # 将被删除的文件名也放入 changed_files 中
+                        deleted_files.append(diff.a_path)  # 记录被删除的文件路径
+    
+    return list(changed_files), deleted_files
 
 
-repo_path = "/Users/yangyan/work/dvdfab_web" # 你的本地git仓库路径
-# repo_path = '/Users/yangyan/work/images'
+# repo_path = "D:\work\images"  # 你的本地git仓库路径
+repo_path = "D:\work\dvdfab_web"  # 你的本地git仓库路径
+# repo_path = "D:\work\streamfab.us"  # 你的本地git仓库路径
+branch = 'pre6'  # 你的分支名
+branch = 'master'  # 你的分支名
+authors = '姚飞扬,秦陈志'  # 提交者名称，用逗号分隔
+# authors = '韩钊,姚飞扬,秦陈志'  # 提交者名称，用逗号分隔
+authors = '阳艳'  # 提交者名称，用逗号分隔
+start_date = '2024-03-25 00:00:00'  # 开始日期时间
+end_date = ''#'2024-02-27 00:00:00'  # 结束日期时间
 
-branch = 'master' # 你的分支名
-authors = '阳艳' # 提交者名称，用逗号分隔
-date = '2023-09-11 00:17:00' # 开始日期时间
+files, deleted_files = list_changed_files_after_date_by_authors(repo_path, branch, authors, start_date, end_date)
 
-files = list_changed_files_after_date_by_authors(repo_path, branch, authors, date)
-#############修改的文件
 if len(files) == 0:
-  print('---------------没有检测出修改的文件，可能是时间的配置需要往前推8个小时，或者更多--------------')
+    print('---------------没有检测出修改的文件--------------')
 else:
-  print('---------------复制的文件：')
-  print(files)
+    print('---------------修改的文件：')
+    print(files)
+
+if len(deleted_files) == 0:
+    print('---------------没有检测出删除的文件--------------')
+else:
+    print('---------------被删除的文件：')
+    print(deleted_files)
+
+
+
 
 #######################复制
 import os
 import shutil
 
 # 初始化源文件夹
-# source_folder_name = '/Users/yangyan/work/dvdfab_web'
 source_folder_name = repo_path
 
 
 # 初始化目标文件夹列表
-##, '/Users/yangyan/work/dvdfab.us'
-target_folder_names = ['/Users/yangyan/work/ja.dvdfab.cn', '/Users/yangyan/work/dvdfab.us', '/Users/yangyan/work/streamfab_us']
-# target_folder_names = ['/Users/yangyan/work/images_us', '/Users/yangyan/work/streamfab_us/static']
+target_folder_names = ['D:\work/dvdfab_web']
+target_folder_names = [ 'D:\work/dvdfab.org']
+# target_folder_names = [ 'D:\work/dvdfab.org','D:\work/dvdfab.us', 'D:\work/streamfab.us']
+# target_folder_names = ['D:\work/ja.dvdfab.cn', 'D:\work/dvdfab.us', 'D:\work/streamfab.us', 'D:\work/ura.dvdfab.org', 'D:\work/dvdfab.org','D:\work/dvdfab_web']
+# target_folder_names = ['D:\work/images_us', 'D:\work/streamfab.us/static']
+# target_folder_names = ['D:\work/streamfab.us/static']
+# target_folder_names = ['D:\work/images1']
 
 # 初始化文件列表
 file_list = files
@@ -66,9 +94,26 @@ for target_folder_name in target_folder_names:
     for file_name in file_list:
         try:
             target_file_name = file_name
-            print(target_file_name,'streamfab' in target_folder_name and 'pages' in target_folder_name)
             if 'streamfab' in target_folder_name and 'pages' in target_file_name:
                target_file_name = file_name.replace('pages', 'pages/main')
+            # 主站同步到streamfab，目录加上pages
+            if 'streamfab' in target_folder_name and 'pages' in target_file_name:
+               target_file_name = file_name.replace('pages', 'pages/main')
+            #streamfab 站的promotion_info同步到主站目录去掉main
+            if 'streamfab' in source_folder_name and 'pages' in file_name:
+               target_file_name = file_name.replace('pages/main', 'pages')
+            # streamfab 站的promotion_info同步到主站改名
+            if 'streamfab' in source_folder_name and 'promotion/promotion_info' in file_name:
+               target_file_name = file_name.replace('promotion_info', 'streamfab_promotion_info')  
+            # streamfab 站的图片同步到主站去掉static
+            if 'images' in target_folder_name and 'static/' in file_name:
+               target_file_name = file_name.replace('static/', '')
+            # 非图片目录不同步
+            if 'images' in target_folder_name and not target_file_name.startswith(("images", "webp")):
+               continue
+            # streamfab站的代码同步到主站忽略图片
+            if 'dvdfab' in target_folder_name and target_file_name.startswith(("static/images", "static/webp")):
+               continue
             source_file = os.path.join(source_folder_name, file_name)
             target_file = os.path.join(target_folder_name, target_file_name)
             os.makedirs(os.path.dirname(target_file), exist_ok=True)
@@ -81,3 +126,31 @@ if len(file_list) == 0:
 else:
   print("复制完成!")
   print("代码提交之前，还是需要仔细review哟!")
+
+
+import os
+from colorama import init, Fore  # 使用colorama库进行彩色输出
+
+init(autoreset=True)  # 初始化colorama，autoreset参数用于自动重置颜色
+
+def delete_files_in_folders(target_folders, files_to_delete):
+    success = True
+
+    for folder_name in target_folders:
+        for file_name in files_to_delete:
+            file_path = os.path.join(folder_name, file_name)
+            try:
+                os.remove(file_path)
+                print(f"成功删除文件: {file_path}")
+            except OSError as e:
+                success = False
+                print(Fore.RED + f"删除文件失败: {file_path}")
+
+    return success
+
+success = delete_files_in_folders(target_folder_names, deleted_files)
+
+if success:
+    print("所有文件删除成功！")
+else:
+    print("部分文件删除失败，请检查输出信息。")
